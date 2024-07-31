cb42f44a3845306ddda5c0e92463a0d97d864a53
thuanCEO <dohuuthuan.bhdn@gmail.com>
2024-07-31 09:44:10 +0700

    create architecture

---
+++ src/utils/routers.js
@@ -0,0 +1,13 @@
+export const ROUTERS = {
+  //Router for manager
+  MANAGEMENT: {
+    MANAGEMENT_DASHBOARD: "/reality3d/management/management-dashboard-page",
+    MANAGEMENT_ACCOUNT: "/reality3d/management/management-accounts-page",
+  },
+
+  //Router for staff
+  STAFF: {
+    STAFF_DASHBOARD: "/reality3d/management/management-dashboard-page",
+    STAFF_ACCOUNT: "/reality3d/management/management-accounts-page",
+  },
+};
---
+++ src/utils/validation.js
@@ -0,0 +1,8 @@
+export const GlobalStyles = {
+  colors: {
+    backgroundColorPrimary100: "#447EFF",
+    backgroundColorPrimary200: "#773BFF",
+    error100: "#fcdcbf",
+    error500: "#f37c13",
+  },
+};
